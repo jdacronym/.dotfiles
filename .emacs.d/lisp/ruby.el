50db45b9a634ab1b87c1511feaceb3eb573b58ee
jdacronym <jdacronym@users.noreply.github.com>
2017-07-02 13:15:52 -0700

    Big update to ruby.el
    - func to find the root of a ruby app: #'ruby-project-root-for-file
      - Apparently can't depend on RAILS_ROOT to exist, so here we are
      - used to run specs from the right directory
        - even when calling #'bundle-exec-rspec interactively or in a non ruby file
      - TODO: make this work for gem projects as well as Rails
    - #'test-file-for-source and #'source-file-for-test
      - given absolute path for a source file, gives the path to the spec file
      - given the same for a spec file, gives the path to the corresponding source
    - #'ruby-complement-filename
      - operates on file path for the current buffer
      - uses the appropriate of the above functions to get path
      - enables switching between test and source with...
    - #'ruby-complementary-file and #'ruby-complementary-file-other-window
      - actually switch you between test and source
      - bound to "C-x s" and "C-x S" respectively with a ruby-mode hook.
    - use #'cl-assert judiciously to prevent regression bugs on  critical functions
    
    	modified:   .emacs.d/lisp/ruby.el

--- .emacs.d/lisp/ruby.el
+++ .emacs.d/lisp/ruby.el
@@ -3,8 +3,104 @@
 (defun bundle-exec-rspec ()
   (interactive)
   (async-shell-command
-   "source ~/.bashrc; bundle exec rspec;"
+   (concat "source ~/.bashrc;"
+	   "cd " (ruby-project-root-for-file (buffer-file-name)) ";"
+	   "bundle exec rspec;")
    "*RSpec*"))
 
 (add-to-list 'ruby-mode-hook (lambda () (interactive)
 			       (local-set-key (kbd "C-x t") #'bundle-exec-rspec)))
+
+(defun ruby-lib-or-app-file-path (filename)
+  (cond ((string-match-p "app/" filename) (replace-regexp-in-string "app\/" "spec/" filename))
+	((string-match-p "spec\/lib\/" filename) (replace-regexp-in-string "spec\/" "" filename))
+	((string-match-p "lib\/" filename) (replace-regexp-in-string "lib\/" "spec/lib/" filename))
+	((string-match-p "spec/" filename) (replace-regexp-in-string "spec\/" "app/" filename))
+	('t (cl-assert (equal 1 nil)))))
+
+;; note: changing *_spec.rb to *.rb and vice-versa happens elsewhere
+(cl-assert (equal (ruby-lib-or-app-file-path "foo/spec/models/example_spec.rb")
+		  "foo/app/models/example_spec.rb"))
+(cl-assert (equal (ruby-lib-or-app-file-path "foo/app/models/example.rb")
+		  "foo/spec/models/example.rb"))
+(cl-assert (equal (ruby-lib-or-app-file-path "foo/spec/lib/example.rb")
+		  "foo/lib/example.rb"))
+(cl-assert (equal (ruby-lib-or-app-file-path "foo/lib/example.rb")
+		  "foo/spec/lib/example.rb"))
+
+(defun ruby-test-file-p (filename)
+  (string-match-p "_spec\.rb" filename))
+
+(defun ruby-source-file-p (filename)
+  (not (ruby-test-file-p filename)))
+
+(defun source-file-for-test (filename)
+  (cond ((ruby-test-file-p filename)
+	 (let ((sans-spec-extension (replace-regexp-in-string "_spec" "" filename)))
+	   (ruby-lib-or-app-file-path sans-spec-extension)))
+	('t (cl-assert (equal '("filename doesn't end in _spec.rb" . filename) nil)))))
+
+(cl-assert (equal (source-file-for-test "foo/spec/models/example_spec.rb")
+		  "foo/app/models/example.rb"))
+(cl-assert (equal (source-file-for-test "foo/spec/lib/example_spec.rb")
+		  "foo/lib/example.rb"))
+
+(defun test-file-for-source (filename)
+  (cond ((ruby-source-file-p filename)
+	 (let ((with-spec-extension (replace-regexp-in-string "\.rb" "_spec.rb" filename)))
+	   (ruby-lib-or-app-file-path with-spec-extension)))
+	('t (cl-assert (equal '("This is a test already!" . filename) nil)))))
+
+(cl-assert (equal (test-file-for-source "foo/app/models/example.rb")
+		  "foo/spec/models/example_spec.rb"))
+(cl-assert (equal (test-file-for-source "foo/lib/example.rb")
+		  "foo/spec/lib/example_spec.rb"))
+
+(defun ruby-complement-filename (filename)
+  (if (ruby-test-file-p filename)
+      (source-file-for-test filename)
+    (test-file-for-source filename)))
+
+(cl-assert (equal (ruby-complement-filename "foo/spec/models/example_spec.rb")
+		  "foo/app/models/example.rb"))
+(cl-assert (equal (ruby-complement-filename "foo/spec/lib/example_spec.rb")
+		  "foo/lib/example.rb"))
+(cl-assert (equal (ruby-complement-filename "foo/app/models/example.rb")
+		  "foo/spec/models/example_spec.rb"))
+(cl-assert (equal (ruby-complement-filename "foo/lib/example.rb")
+		  "foo/spec/lib/example_spec.rb"))
+
+(defun ruby-complementary-file ()
+  (interactive)
+  (let ((fname (ruby-complement-filename (buffer-file-name))))
+    (find-file fname)))
+
+(defun ruby-complementary-file-other-window ()
+  (interactive)
+  (let ((fname (ruby-complement-filename (buffer-file-name))))
+    (find-file-other-window fname)))
+
+(add-to-list 'ruby-mode-hook (lambda () (interactive)
+			       (local-set-key (kbd "C-x s") #'ruby-complementary-file)))
+(add-to-list 'ruby-mode-hook (lambda () (interactive)
+			       (local-set-key (kbd "C-x S") #'ruby-complementary-file-other-window)))
+
+(defvar ruby-last-good-root ""
+  "The last good value for project root")
+(defun ruby-project-root-for-file (filename)
+  (setq ruby-last-good-root
+	(cond ((null filename) ruby-last-good-root)
+	      ((string-match-p "spec\/" filename) (replace-regexp-in-string "spec\/.*" "" filename))
+	      ((string-match-p "app\/" filename) (replace-regexp-in-string "app\/.*" "" filename))
+	      ((string-match-p "lib\/" filename) (replace-regexp-in-string "lib\/.*" "" filename))
+	      ('t filename))))
+
+(cl-assert (equal (ruby-project-root-for-file
+		   "/usr/home/foobar/src/some-project/spec/models/some_model_spec.rb")
+		  "/usr/home/foobar/src/some-project/"))
+(cl-assert (equal (ruby-project-root-for-file
+		   "/usr/home/foobar/src/some-project/app/models/some_model.rb")
+		  "/usr/home/foobar/src/some-project/"))
+(cl-assert (equal (ruby-project-root-for-file
+		   "/usr/home/foobar/src/some-project/lib/services/some_service.rb")
+		  "/usr/home/foobar/src/some-project/"))
